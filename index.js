6f0eb403dfd3745d1827cca1044ecd22bd5055fb
Tess <tess.mueske@gmail.com>
2024-01-28 18:27:27 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+const findMatching = function(array, string){
+    let drivers = []
+    for (let i = 0; i < array.length; i++) {
+        let driverName = array[i];
+        if (driverName.toLowerCase() === string.toLowerCase())
+            drivers.push(driverName);
+}
+return drivers;
+}
+
+const fuzzyMatch = function(array, string){
+    let matchingDrivers = array.filter(driver => driver.startsWith(string));
+    return matchingDrivers;
+}
+
+const matchName = function(array, string){
+    return array.filter(driver => driver.name === string);
+}
